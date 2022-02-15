7f94253a497a2853ef4a0ea02b9d758e9e2d85ca
Rizkyamanah <januaranafi@gmail.com>
2022-02-15 08:40:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/profilsekolah/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.profilsekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void profil(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, guruActivity.class);
+        startActivity(intent);
+    }
+
+    public void eskul(View view) {
+        Intent intent = new Intent(MainActivity.this, eskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, fasilitasActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, perpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, galeriActivity.class);
+        startActivity(intent);
+    }
+}
